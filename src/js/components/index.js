756d03ef1bddfe35cd29d6096580c0254c5cc521
Solocomrade1101 <solocomrade@mail.ru>
2024-06-08 13:03:16 +0300

    remove preloader

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -5,7 +5,7 @@ if (street) setTimeout(initStreet, 0)
 function initStreet () {
   street.addEventListener("click", () => {
     const address = street.textContent
-    const mapUrl = `https://www.google.com/maps?q=${encodeURIComponent(address)}`;
-    window.open(mapUrl, '_blank');
+    const mapUrl = `https://www.google.com/maps?q=${encodeURIComponent(address)}`
+    window.open(mapUrl, '_blank')
   })
 }
--- src/js/components/preload.js
+++ src/js/components/preload.js
@@ -1,14 +1,14 @@
-import gsap from 'gsap';
-window.addEventListener('load', function() {
-  setTimeout(function() {
-    const preloader = document.getElementById('preloader')
-    const mainContent = document.getElementById('main-content')
-
-    gsap.to(preloader, { duration: 1, opacity: 0, onComplete: function() {
-        preloader.style.display = 'none'
-      }})
-
-    mainContent.style.display = 'block'
-    gsap.fromTo(mainContent, { opacity: 0 }, { delay: .5, duration: 1, opacity: 1 })
-  }, 2000)
-});
+// import gsap from 'gsap'
+// window.addEventListener('load', function() {
+//   setTimeout(function() {
+//     const preloader = document.getElementById('preloader')
+//     const mainContent = document.getElementById('main-content')
+//
+//     gsap.to(preloader, { duration: 1, opacity: 0, onComplete: function() {
+//         preloader.style.display = 'none'
+//       }})
+//
+//     mainContent.style.display = 'block'
+//     gsap.fromTo(mainContent, { opacity: 0 }, { delay: .5, duration: 1, opacity: 1 })
+//   }, 2000)
+// })
